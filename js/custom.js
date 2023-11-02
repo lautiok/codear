a86b9b0f941eecef6a79a08956ab252a039ef47b
Nahuel Guerra <Nahuelguerra56b@gmail.com>
2023-11-01 22:15:31 -0300

    init

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+const check = document.querySelector('.check');
+check.addEventListener('click', idioma);
+
+function idioma() {
+  let id = check.checked;
+  if (id) {
+    location.href = "../es/index.html";
+  } else {
+    location.href = "../index.html";
+  }
+}
+
+$(document).ready(function() {
+  $("#project-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+  });
+});
+
+(function($) {
+  "use strict";
+
+  // COLOR MODE
+  $('.color-mode').click(function() {
+    $('.color-mode-icon').toggleClass('active');
+    $('body').toggleClass('dark-mode');
+  });
+
+  // SMOOTHSCROLL
+  $('.nav-link, .custom-btn-link').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top - 49
+    }, 1000);
+    event.preventDefault();
+  });
+
+  // TOOLTIP
+  $('.social-links a').tooltip();
+
+})(jQuery);
