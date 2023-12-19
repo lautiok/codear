2665452f126e38ed526aaa50d8b8e4540bdd9dc6
Nahuel Guerra <Nahuelguerra56b@gmail.com>
2023-12-19 17:36:48 -0300

    codeardeploy

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,12 @@
-const checks = document.querySelector('.check');
-checks.addEventListener('click', idioma);
+// Función para cambiar la ubicación de la página según el estado del checkbox
+const checkbox = document.querySelector('.check');
+checkbox.addEventListener('click', () => {
+  const isChecked = checkbox.checked;
+  location.href = isChecked ? '../es/index.html' : '../index.html';
+});
 
-function idioma() {
-  let id = checks.checked;
-  if (id) {
-    location.href = "../es/index.html";
-  } else {
-    location.href = "../index.html";
-  }
-}
-
-$(document).ready(function() {
+// Inicialización del plugin Owl Carousel
+$(document).ready(() => {
   $("#project-carousel").owlCarousel({
     items: 1,
     loop: true,
@@ -18,27 +14,26 @@ $(document).ready(function() {
     autoplayTimeout: 2000,
     autoplayHoverPause: true
   });
-});
 
-(function($) {
-  "use strict";
-
-  // COLOR MODE
-  $('.color-mode').click(function() {
+  // Cambiar el modo de color
+  $('.color-mode').click(() => {
     $('.color-mode-icon').toggleClass('active');
     $('body').toggleClass('dark-mode');
   });
 
-  // SMOOTHSCROLL
+  // Desplazamiento suave y manejo de errores
   $('.nav-link, .custom-btn-link').on('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top - 49
-    }, 1000);
-    event.preventDefault();
+    try {
+      event.preventDefault();
+      const $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+    } catch (error) {
+      console.error('Error en el desplazamiento suave:', error);
+    }
   });
 
-  // TOOLTIP
+  // Agregar tooltips a los enlaces sociales
   $('.social-links a').tooltip();
-
-})(jQuery);
+});
