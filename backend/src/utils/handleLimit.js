f3d9b5e16219eb2b884a25c6aa5defa80c7d8d54
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-08-16 12:27:57 -0300

    delate backend

--- backend/src/utils/handleLimit.js
+++
@@ -1,7 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const limit = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: "Demasiadas solicitudes de esta IP. Vuelva a intentarlo después de una hora.",
-})
